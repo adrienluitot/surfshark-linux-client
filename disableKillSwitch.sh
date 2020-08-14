18e3094e4bc181a8668ed35034a6ad5b43dc902e
JonahStrotmann <39196875+JonahStrotmann@users.noreply.github.com>
2020-08-14 17:04:42 +0200

    Delete disableKillSwitch.sh

--- disableKillSwitch.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-echo 'This will reset all previous rules are you sure that you want to proceed?'
-sudo ufw reset
-sudo ufw default deny incoming
-sudo ufw default allow outgoing
-sudo ufw enable
